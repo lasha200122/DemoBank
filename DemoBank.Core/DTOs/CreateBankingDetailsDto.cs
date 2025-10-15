7d98854bf22c75dd9785a3a5952b58af1fe6366d
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-10-15 15:25:41 +0400

    make banking details optional

--- DemoBank.Core/DTOs/CreateBankingDetailsDto.cs
+++ DemoBank.Core/DTOs/CreateBankingDetailsDto.cs
@@ -16,26 +16,26 @@ namespace DemoBank.Core.DTOs
     public class CardPaymentDetails
     {
         [CreditCard]
-        public string CardNumber { get; set; }
+        public string? CardNumber { get; set; }
 
         [MaxLength(100)]
-        public string CardHolderName { get; set; }
+        public string? CardHolderName { get; set; }
 
         [RegularExpression(@"^(0[1-9]|1[0-2])\/\d{2}$")]
-        public string ExpiryDate { get; set; } // MM/YY format
+        public string? ExpiryDate { get; set; } // MM/YY format
 
         [RegularExpression(@"^\d{3,4}$")]
-        public string CVV { get; set; }
+        public string? CVV { get; set; }
     }
 
     public class BankAccountDetails
     {
-        public string AccountNumber { get; set; }
+        public string? AccountNumber { get; set; }
 
-        public string RoutingNumber { get; set; }
+        public string? RoutingNumber { get; set; }
 
         [MaxLength(100)]
-        public string AccountHolderName { get; set; }
+        public string? AccountHolderName { get; set; }
 
     }
 
@@ -44,28 +44,28 @@ namespace DemoBank.Core.DTOs
         [MaxLength(100)]
         [RegularExpression(@"^(0x[a-fA-F0-9]{40}|[13][a-km-zA-HJ-NP-Z1-9]{25,34}|bc1[a-zA-HJ-NP-Z0-9]{11,71})$",
             ErrorMessage = "Invalid wallet address format.")]
-        public string WalletAddress { get; set; }
+        public string? WalletAddress { get; set; }
 
         [MaxLength(100)]
         [RegularExpression(@"^[A-Fa-f0-9]{64}$",
             ErrorMessage = "Invalid transaction hash format (must be a 64-character hex string).")]
-        public string TransactionHash { get; set; }
+        public string? TransactionHash { get; set; }
     }
 
     public class IbanDetails
     {
         [MaxLength(100)]
-        public string BeneficialName { get; set; }
+        public string? BeneficialName { get; set; }
 
         [MaxLength(34)]
         [RegularExpression(@"^[A-Z]{2}\d{2}[A-Z0-9]{1,30}$", ErrorMessage = "Invalid IBAN format.")]
-        public string IBAN { get; set; }
+        public string? IBAN { get; set; }
 
         [MaxLength(100)]
-        public string Reference { get; set; }
+        public string? Reference { get; set; }
 
         [MaxLength(11)]
         [RegularExpression(@"^[A-Z]{4}[A-Z]{2}[A-Z0-9]{2}([A-Z0-9]{3})?$", ErrorMessage = "Invalid BIC/SWIFT code.")]
-        public string BIC { get; set; }
+        public string? BIC { get; set; }
     }
 }
