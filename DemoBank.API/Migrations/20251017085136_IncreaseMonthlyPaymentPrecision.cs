d5132efab02fe7bc804b16c6efa690d6521a5408
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-10-17 12:53:16 +0400

    increase monthly payment precision

---
+++ DemoBank.API/Migrations/20251017085136_IncreaseMonthlyPaymentPrecision.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DemoBank.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class IncreaseMonthlyPaymentPrecision : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "MonthlyPayment",
+                table: "Loans",
+                type: "numeric(18,8)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(18,6)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "MonthlyPayment",
+                table: "Loans",
+                type: "numeric(18,6)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(18,8)");
+        }
+    }
+}
